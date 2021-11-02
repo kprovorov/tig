ace5e8478726a29cb5c1609ea1d9b3c803ad3df8
Kirill Provorov <kirill@provorov.dev>
2021-11-03 00:58:33 +0200

    simple app

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express')
+const {Client} = require('pg')
+const faker = require('faker');
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', async (req, res) => {
+
+  const client = new Client({
+    user: 'admin',
+    host: '127.0.0.1',
+    database: 'postgres',
+    password: 'admin',
+    port: 5432,
+  })
+  await client.connect()
+  const user = await client.query('INSERT INTO users (name, age, email) VALUES ($1, $2, $3) RETURNING *', [
+    faker.name.findName(),
+    faker.datatype.number(70),
+    faker.internet.email()
+  ])
+  await client.end()
+
+  res.send(user.rows[0])
 })
 
 app.listen(port, () => {
